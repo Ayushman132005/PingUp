8310594dae8fa318796078ccf34d024baf5825d8
Your Name <you@example.com>
2026-01-21 20:45:52 +0530

    frontend changes

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../App.css';
+
+export default function LandingPage() {
+  return (
+    <div className="landingPageContainer">
+        <nav>
+            <div className="navHeader">
+                <h2>PingUp</h2>
+            </div>
+            <div className="navlist">
+                <p>Join as Guest</p>
+                <p>Register</p>
+                <div role='button'>
+                    <p>Login</p>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
